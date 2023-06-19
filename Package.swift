ae1ec79da3aadf2bdfbc42cfd5f5aa1ba062f0a2
Harry <miniwing.hz@gmail.com>
2023-06-20 01:35:58 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
             targets: ["YoutubeDL"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/miniwing/FFmpeg-iOS.git", .branch("main")),
-        .package(url: "https://github.com/pvieito/PythonKit.git", .branch("master")),
-        .package(url: "https://github.com/miniwing/Python-iOS.git", .branch("kivy-ios")),
+        .package(url: "https://github.com/miniwing/FFmpeg-iOS.git", .revision("a5b268bd648f0d61179ee04aefdcbcc91cc81e5c")),
+        .package(url: "https://github.com/miniwing/PythonKit.git", .branch("master")),
+        .package(url: "https://github.com/miniwing/Python-iOS.git", .revision("1fa595f162b6df0dd729304ac284de8cc31b84f5")),
     ],
     targets: [
         .target(
